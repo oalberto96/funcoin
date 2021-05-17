c3c65ebb5edb92a64f764eaa936966c77f7352c3
oalberto96 <ochoapublic@gmail.com>
2021-05-16 21:54:49 -0700

    [add] blockchain class

---
+++ funcoin/blockchain.py
@@ -0,0 +1,42 @@
+import json
+from datetime import datetime
+from hashlib import sha256
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.pending_transactions = []
+        print('Creating genesis block')
+        self.new_block()
+
+    def new_block(self, previous_hash=None):
+        block = {
+            'index': len(self.chain),
+            'timestamp': datetime.utcnow().isoformat(),
+            'transactions': self.pending_transactions,
+            'previous_hash': previous_hash,
+        }
+        block_hash = self.hash(block)
+        block['hash'] = block_hash
+
+        self.pending_transactions = []
+        self.chain.append(block)
+
+        print(f'Created block {block["index"]}')
+        return block
+
+    
+    @staticmethod
+    def hash(block):
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return sha256(block_string).hexdigest()
+    
+    def last_block(self):
+        return self.chain[-1] if self.chain else None
+
+    def new_transaction(self, sender, recipient, ammount):
+        self.pending_transactions.append({
+            'recipient': recipient,
+            'sender': sender,
+            'amount': amount,
+        })
